495b56aacf277ef7731b3107537c2f76e70e0b27
Dhanalakshmi C <dhana2552@gmail.com>
2022-03-01 22:51:26 +0530

    Add files via upload

---
+++ 04_backward_prop.py
@@ -0,0 +1,14 @@
+import torch
+x = torch.tensor([1.0])
+y = torch.tensor([2.0])
+
+w = torch.tensor([1.0], requires_grad=True)
+y_hat = w*x
+
+#forward pass
+loss = (y_hat - y)**2
+print(loss)
+
+#backward pass
+loss.backward()
+print(w.grad)
